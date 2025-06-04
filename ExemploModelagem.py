fe648038c71526510c7051e541850da3bc9ffdfd
AndreMGurgel <85973765+AndreMGurgel@users.noreply.github.com>
2025-06-04 11:29:23 -0300

    Add files via upload

---
+++ ExemploModelagem.py
@@ -0,0 +1,8 @@
+total = {"Ana", "João", "Maria", "Carlos"}
+presentes = {"Ana", "João"}
+
+faltaram = total - presentes
+
+print("Faltaram:", faltaram)
+print("Presentes:", len(presentes))
+print("Faltaram:", len(faltaram))
